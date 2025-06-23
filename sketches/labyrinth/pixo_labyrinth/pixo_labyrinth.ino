5cb7013bcf30548c5b4abd08af6af9fa9355a64d
Elneto <elnetociprian@gmail.com>
2025-06-23 23:58:08 +0200

    Recursive backtracking algorithm
    Implemented maze generation using a recursive backtracking algorithm, each reset creates a new random maze.
    # addition and cleanup of constants
    # carveMaze(), shuffleDirections() and isInBounds() added

--- sketches/labyrinth/pixo_labyrinth/pixo_labyrinth.ino
+++ sketches/labyrinth/pixo_labyrinth/pixo_labyrinth.ino
@@ -7,20 +7,29 @@
 #include "I2Cdev.h"
 #include "MPU6050.h"
 
-#define DATAPIN    19
-#define CLOCKPIN   18
-#define BRIGHTNESS 30
-#define MAZE_SIZE 16
+#define DATAPIN       19
+#define CLOCKPIN      18
+#define MATRIX_WIDTH  16
+#define MATRIX_HEIGHT 16
+#define BRIGHTNESS    30
 
 Adafruit_DotStarMatrix pixo = Adafruit_DotStarMatrix(
-  16, 16, DATAPIN, CLOCKPIN,
-  DS_MATRIX_TOP    + DS_MATRIX_LEFT +
+  MATRIX_WIDTH, MATRIX_HEIGHT, DATAPIN, CLOCKPIN,
+  DS_MATRIX_TOP + DS_MATRIX_LEFT +
   DS_MATRIX_ROWS + DS_MATRIX_PROGRESSIVE,
   DOTSTAR_BGR
 );
 
-uint8_t labyrinth[MAZE_SIZE][MAZE_SIZE];
 MPU6050 IMU;
+
+uint8_t labyrinth[MATRIX_WIDTH][MATRIX_HEIGHT];
+// axes movement     UP,RI,DO,LE
+int direction_x[] = { 0, 1, 0, -1};
+int direction_y[] = {-1, 0, 1,  0};
+#define UP    0
+#define RIGHT 1
+#define DOWN  2
+#define LEFT  3
 
 void setup() {
   Serial.begin(115200);
@@ -45,9 +54,9 @@ void loop() {
   // clrscr()
   pixo.fillScreen(0);
 
-  // Draw generated maze, walls (0s), gray
-  for (int x = 0; x < MAZE_SIZE; x++) {
-    for (int y = 0; y < MAZE_SIZE; y++) {
+  // Draw generated maze, walls(0) in gray
+  for (int x = 0; x < MATRIX_WIDTH; x++) {
+    for (int y = 0; y < MATRIX_HEIGHT; y++) {
       if (labyrinth[x][y] == 0){
         pixo.drawPixel(x, y, pixo.Color(200, 200, 200));
       }
@@ -58,22 +67,64 @@ void loop() {
 }
 
 void generateMaze() {
-  // Fill with walls (0s)
-  for (int x = 0; x < MAZE_SIZE; x++) {
-    for (int y = 0; y < MAZE_SIZE; y++) {
+  // Fill with walls(0)
+  for (int x = 0; x < MATRIX_WIDTH; x++) {
+    for (int y = 0; y < MATRIX_HEIGHT; y++) {
       labyrinth[x][y] = 0;
     }
   }
 
-  // Start from a cell around the middle of the matrix
+  // Start from a pixel around the middle of the matrix
   int startX = random(6, 10);
   int startY = random(6, 10);
   labyrinth[startX][startY] = 1;
   Serial.println("Center: ("+String(startX)+", "+String(startY)+")");
 
-  // clrscr() and briefly show the starting cell
+  carveMaze(startX, startY);
+
+  // clrscr() and briefly show the starting pixel
   pixo.fillScreen(0);
   pixo.drawPixel(startX, startY, pixo.Color(0, 255, 0));
   pixo.show();
   delay(1000);
+}
+
+// --------------------------------------------------------------------
+// Recursive backtracking algorithm for maze generation.
+// This particular version jumps two pixels from current as we will be
+// using the LEDs of the matrix as walls and paths. PIXEL ON=wall/OFF=path.
+// As a side effect, due to the 16x16 matrix, depending on the starting
+// pixel, there will be double walls at two edges for each maze generated.
+// Source: https://weblog.jamisbuck.org/2010/12/27/maze-generation-recursive-backtracking
+// --------------------------------------------------------------------
+void carveMaze(int current_x, int current_y) {
+  int directions[4] = {UP, DOWN, RIGHT, LEFT};
+  shuffleDirections(directions);
+
+  for (int i = 0; i < 4; i++) {
+    Serial.print(directions[i]);
+    // For each direction, it checks two pixels away
+    int next_x = current_x + direction_x[directions[i]] * 2;
+    int next_y = current_y + direction_y[directions[i]] * 2;
+    Serial.println(": nx: "+String(next_x)+"/ny: "+String(next_y));
+    // to see if it is still within bounds and unvisited
+    if (isInBounds(next_x, next_y) && labyrinth[next_x][next_y] == 0) {
+      labyrinth[next_x][next_y] = 1;
+      labyrinth[current_x + direction_x[directions[i]]][current_y + direction_y[directions[i]]] = 1;
+      carveMaze(next_x, next_y);
+    }
+  }
+}
+
+void shuffleDirections(int* dirs) {
+  for (int i = 0; i < 4; i++) {
+    int r = random(4);
+    int temp = dirs[i];
+    dirs[i] = dirs[r];
+    dirs[r] = temp;
+  }
+}
+
+bool isInBounds(int x, int y) {
+  return x > 0 && x < MATRIX_WIDTH - 1 && y > 0 && y < MATRIX_HEIGHT - 1;
 }
